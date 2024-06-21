4e9b5a3cc346f15d4da2738697bc3b71e7ae6abd
votex001 <jeneea45@gmail.com>
2024-06-21 17:51:11 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    host: true,
+    open: '/#/',
+  },
+  base: './',
+  publicPath: '/assets/', // Add this line to specify the public path for your assets
 })
