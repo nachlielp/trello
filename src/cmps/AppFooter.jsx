88ae33b09a782500527c4168f017d198b5d7a8b9
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-06-22 11:04:14 +0300

    cleanup footer

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,15 +1,10 @@
 
-import { useSelector } from 'react-redux'
 import { UserMsg } from './UserMsg.jsx'
 
 export function AppFooter() {
-    const count = useSelector(storeState => storeState.userModule.count)
 
     return (
         <footer className="app-footer">
-            <p>
-                coffeerights - count: {count}
-            </p>
             <UserMsg />
         </footer>
     )
