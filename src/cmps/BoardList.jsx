b95b4eb529f2e1bfe9b479c15f8686ce369820b4
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-06-24 16:08:44 +0300

    board list header

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -1,11 +1,14 @@
-import { Card } from "antd"
+import { Card, Button } from "antd"
 import { ListCard } from "./ListCard"
-import { Typography } from "antd"
+import { EllipsisOutlined } from "@ant-design/icons"
 
 export function BoardList({ list, cards }) {
     return (
         <Card className="board-list custom-card">
-            <p className="list-title">{list.name}</p>
+            <header className="board-list-header">
+                <p className="list-title">{list.name}</p>
+                <Button className="list-more-btn" size="small"><EllipsisOutlined /></Button>
+            </header>
             {cards.map(card => <ListCard key={card.id} card={card} />)}
         </Card>
     )
