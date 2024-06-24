efbae00654b79daf986bc9927f91b65a081cbc3d
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-06-24 09:44:08 +0300

    Set mock default. board

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -2,39 +2,50 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useSelector } from 'react-redux'
-import { loadBoards, addBoard, updateBoard, removeBoard, addBoardMsg } from '../store/trello.actions'
+import { loadBoard, addBoard, updateBoard, removeBoard, addBoardMsg } from '../store/trello.actions'
 
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { userService } from '../services/user.service'
 import { boardService } from '../services/board.service.local'
+import { BoardHeader } from '../cmps/BoardHeader'
+import { BoardList } from '../cmps/BoardList'
+
+const DEFAULT_BOARD_ID = "64f721f169b14894fac36cdf";
 
 export function BoardIndex() {
 
-    const boards = useSelector(storeState => storeState.boardModule.boards)
+    const board = useSelector(storeState => storeState.boardModule.boards.find(board => board.id === DEFAULT_BOARD_ID))
+    const lists = useSelector(storeState => storeState.boardModule.lists.find(list => list.boardId === DEFAULT_BOARD_ID))
+    const members = useSelector(storeState => storeState.boardModule.members.find(member => member.boardId === DEFAULT_BOARD_ID))
+    const cards = useSelector(storeState => storeState.boardModule.cards.find(card => card.boardId === DEFAULT_BOARD_ID))
+    console.log(board)
+    console.log(lists)
+    console.log(members)
+    console.log(cards)
 
     useEffect(() => {
-        loadBoards()
+        // loadBoard(DEFAULT_BOARD_ID)
     }, [])
 
-    async function onRemoveBoard(boardId) {
-        try {
-            await removeBoard(boardId)
-            showSuccessMsg('Board removed')
-        } catch (err) {
-            showErrorMsg('Cannot remove board')
-        }
-    }
+    // async function onRemoveBoard(boardId) {
+    //     try {
+    //         await removeBoard(boardId)
+    //         showSuccessMsg('Board removed')
+    //     } catch (err) {
+    //         showErrorMsg('Cannot remove board')
+    //     }
+    // }
 
-    async function onAddBoard() {
-        const board = boardService.getDemoBoard()
-        board.title = prompt('Title?')
-        try {
-            const savedBoard = await addBoard(board)
-            showSuccessMsg(`Board added (id: ${savedBoard._id})`)
-        } catch (err) {
-            showErrorMsg('Cannot add board')
-        }
-    }
+    // async function onAddBoard() {
+    //     const board = boardService.getDemoBoard()
+    //     board.title = prompt('Title?')
+    //     try {
+    //         const savedBoard = await addBoard(board)
+    //         showSuccessMsg(`Board added (id: ${savedBoard._id})`)
+    //     } catch (err) {
+    //         showErrorMsg('Cannot add board')
+    //     }
+    // }
 
     async function onUpdateBoard(board) {
         const title = prompt('New title?', board.title)
@@ -60,26 +71,9 @@ export function BoardIndex() {
 
     return (
         <section className="board-index">
-            <h3>Boards App</h3>
-            <main>
-                <button onClick={onAddBoard}>Add Board ⛐</button>
-                <ul className="board-list">
-                    {boards.map(board =>
-                        <li className="board-preview" key={board._id}>
-                            <Link to={`${board._id}`}>
-                                <h4>{board.title}</h4>
-                            </Link>
-                            <h1>⛐</h1>
-                            <p>Owner: <span>{board.owner && board.owner.fullname}</span></p>
-                            <hr />
-
-                            {shouldShowActionBtns(board) && <div>
-                                <button onClick={() => { onRemoveBoard(board._id) }}>x</button>
-                                <button onClick={() => { onUpdateBoard(board) }}>Edit</button>
-                            </div>}
-                        </li>)
-                    }
-                </ul>
+            <BoardHeader board={board} />
+            <main className="board-lists">
+                {lists.lists.map(list => <BoardList key={list.id} list={list} cards={cards.cards.filter(card => card.idList === list.id)} />)}
             </main>
         </section>
     )
