152437b170281deccaaecb4dc41aadedce94bdbc
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-06-24 22:46:37 +0300

    remove un used methods

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -6,13 +6,8 @@ import { userService } from '../services/user.service'
 import { boardService } from '../services/board.service.local'
 import { BoardHeader } from '../cmps/BoardHeader'
 import { BoardList } from '../cmps/BoardList'
-import { addLists, addCards, addMembers, addBoard, updateBoard, } from '../store/trello.actions'
-import { store } from '../store/store'
 import { loadTrelloData } from '../store/trello.actions'
 export function BoardIndex() {
-
-    const [loading, setLoading] = useState(true)
-
 
     const lists = useSelector(state => state.boardModule.lists)
     const cards = useSelector(state => state.boardModule.cards)
@@ -23,33 +18,12 @@ export function BoardIndex() {
         loadTrelloData()
     }, []);
 
-    async function onUpdateBoard(board) {
-        const title = prompt('New title?', board.title)
-        if (!title) return
-
-        const boardToSave = { ...board, title }
-        try {
-            const savedBoard = await updateBoard(boardToSave)
-            showSuccessMsg(`Board updated, new title: ${savedBoard.title}`)
-        } catch (err) {
-            showErrorMsg('Cannot update board')
-        }
-    }
-
-    function shouldShowActionBtns(board) {
-        return true
-        const user = userService.getLoggedinUser()
-        if (!user) return false
-        if (user.isAdmin) return true
-        return board.owner?._id === user._id
-    }
-
     return (
         <section className="board-index">
             <>
                 {board && <BoardHeader board={board} />}
                 <main className="board-lists">
-                    {lists && lists.map(list =>
+                    {lists.map(list =>
                         <BoardList key={list.id} list={list} cards={cards.filter(card => card.idList === list.id)} />
                     )}
                 </main>
