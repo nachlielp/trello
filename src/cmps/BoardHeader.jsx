718fc7778a25ea8e3197b0a0d2c48a20ea08300c
votex001 <jeneea45@gmail.com>
2024-06-27 21:39:01 +0300

    Delited useless code

--- src/cmps/BoardHeader.jsx
+++ src/cmps/BoardHeader.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import visibilityIcon from "/img/headerImgs/visibilityIcon.svg";
-import boardIcon from "/img/headerImgs/boardIcon.svg";
 import {
   DownOutlined,
   StarOutlined,
@@ -10,67 +8,69 @@ import {
   EllipsisOutlined,
 } from "@ant-design/icons";
 import { IoFilterSharp } from "react-icons/io5";
-import { LetteredAvatar } from "./LetteredAvatar";
-import { login } from "../store/user.actions";
+import { loadUsers, updateUser } from "../store/user.actions";
 import { UserAvatar } from "./UserAvatar";
+import { VisibilityButton } from "./BoardHeader-cmps/VisibilityButton";
+
+import { ReactSVG } from "react-svg";
+import boardIcon from "/img/headerImgs/boardIcon.svg";
 
 export function BoardHeader() {
   const [isStarredBoard, setIsStarredBoard] = useState(false);
   const [hover, setHover] = useState(false);
   const members = useSelector((state) => state.boardModule.members);
   const board = useSelector((state) => state.boardModule.board);
-  const starFilled = isStarredBoard ? <StarFilled /> : <StarOutlined />;
-  const starEmpty = isStarredBoard ? <StarOutlined /> : <StarFilled />;
-
-  const apiKey = import.meta.env.VITE_TRELLO_API_KEY;
-  const token = import.meta.env.VITE_TRELLO_TOKEN;
+  const currentUser = useSelector((state) =>
+    state.userModule.users?.find(
+      (user) => (user.id = "666fe4efda8643029b6710f3")
+    )
+  );
 
   useEffect(() => {
-    fetchUserId();
-  }, [board]);
+    loadUsers();
+    if (currentUser?.starredBoardIds?.includes(board.id)) {
+      setIsStarredBoard(true);
+    }
+  }, []);
 
-  async function fetchUserId() {
-    const data = await fetch(
-      `https://api.trello.com/1/members/me?key=${
-        import.meta.env.VITE_TRELLO_API_KEY
-      }&token=${token}`
-    );
-    const boardData = await data.json();
-    fetchStarredBoards(boardData.id);
-  }
-
-  async function fetchStarredBoards(id) {
-    try {
-      const data = await fetch(
-        `https://trello.com/1/member/${id}/boardStars?key=${apiKey}&token=${token}`
+  function onStarClick() {
+    if (currentUser?.starredBoardIds?.includes(board.id)) {
+      const newUser = currentUser;
+      newUser.starredBoardIds = currentUser.starredBoardIds.filter(
+        (id) => id !== board.id
       );
-      const boardData = await data.json();
-      setIsStarredBoard(boardData.some((obj) => obj.idBoard === board.id));
-    } catch (err) {
-      console.log(err);
+      updateUser(newUser);
+    } else {
+      const newUser = currentUser;
+      newUser?.starredBoardIds.push(board.id);
+      updateUser(newUser);
+    }
+    if (currentUser?.starredBoardIds?.includes(board.id)) {
+      setIsStarredBoard(true);
+    } else {
+      setIsStarredBoard(false);
     }
   }
-  console.log(members);
+
   return (
     <div className="board-header">
       <div className="left-info">
         {" "}
         <h3 className="board-name">{board.name}</h3>
         <button
-          className="star-icon"
+          className="star-btn"
           onMouseOver={() => setHover(true)}
           onMouseOut={() => setHover(false)}
+          onClick={onStarClick}
         >
           {(hover && (isStarredBoard ? <StarOutlined /> : <StarFilled />)) ||
             (isStarredBoard ? <StarFilled /> : <StarOutlined />)}
         </button>
-        <button className="visibility-icon">
-          <img src={visibilityIcon} />
-        </button>
+        <VisibilityButton />
         <button className="board-btn">
-          <img src={boardIcon} />
+          <ReactSVG src={boardIcon} wrapper="span" className="board-icon" />
           Board
-          <DownOutlined />
+          <DownOutlined className="arow"/>
         </button>
       </div>
       <div className="right-info">
@@ -89,7 +89,7 @@ export function BoardHeader() {
           ))}
         </div>
         <button className="share-btn">
-          <UserAddOutlined className="share-icon"/>
+          <UserAddOutlined className="share-icon" />
           <span className="txt">Share</span>
         </button>
         <button className="dots">
