eff2dfa55a3c3a65ca88534f73831a50c8c0e80e
votex001 <jeneea45@gmail.com>
2024-06-29 16:15:45 +0300

    smarter component

--- src/cmps/UserAvatar.jsx
+++ src/cmps/UserAvatar.jsx
@@ -2,15 +2,34 @@ import { Tooltip } from "antd";
 import { Avatar } from "antd";
 import { utilService } from "../services/util.service";
 
-export function UserAvatar({ member,size=24,...other }) {
-    return (
-        <Tooltip placement="bottom" title={member.fullName} arrow={false} >
-            <Avatar
-                key={member.id} src={member.avatarHash}
-                style={{ backgroundColor: utilService.stringToColor(member.id) }} size={size} {...other}>
-                {utilService.capitalizeInitials(member.fullName)}
-               
-            </Avatar>
-        </Tooltip>
-    )
+import { ReactSVG } from "react-svg";
+import defaultProfile from "/img/defaultProfile.svg";
+
+export function UserAvatar({
+  member,
+  size = 24,
+  src = defaultProfile,
+  img,
+  style = {},
+  ...other
+}) {
+  const dynamicStyles = member
+    ? { backgroundColor: utilService.stringToColor(member.id) }
+    : {};
+
+  return (
+    <Tooltip placement="bottom" title={member?.fullName} arrow={false}>
+      <Avatar
+        key={member?.id}
+        src={member?.avatarHash}
+        style={{ ...dynamicStyles, ...style }}
+        size={size}
+        {...other}
+      >
+        {member && utilService.capitalizeInitials(member?.fullName)}
+        {!member && <ReactSVG src={src} />}
+        {img}
+      </Avatar>
+    </Tooltip>
+  );
 }
