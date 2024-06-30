f0f7821bd8263115440b666cafce2193f8fe0bad
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-07-01 00:26:56 +0300

    Add card preview action buttons

--- src/cmps/Task/TaskPreviewEditModal.jsx
+++ src/cmps/Task/TaskPreviewEditModal.jsx
@@ -1,10 +1,18 @@
 import { useRef, useState } from 'react';
-import { Modal } from 'antd';
+import { Modal, Input, Button } from 'antd';
 import { ReactSVG } from "react-svg"
 import editSvg from '../../assets/svgs/edit.svg';
-import { Input } from "antd"
 import { TaskPreviewBadges } from "./TaskPreviewBadges";
 import { TaskPreviewLabel } from "./TaskPreviewLabel";
+
+import cardIcon from '/img/taskActionBtns/cardIcon.svg';
+import moreIcon from '/img/taskActionBtns/moreIcon.svg';
+import labelIcon from '/img/taskActionBtns/labelIcon.svg';
+import userIcon from '/img/taskActionBtns/userIcon.svg';
+import timeIcon from '/img/taskActionBtns/timeIcon.svg';
+import coverIcon from '/img/taskActionBtns/coverIcon.svg';
+import copyIcon from '/img/taskActionBtns/copyIcon.svg';
+import archiveIcon from '/img/taskActionBtns/archiveIcon.svg';
 
 const { TextArea } = Input;
 
@@ -35,6 +43,17 @@ export function TaskPreviewEditModal({ task, isHovered, editTask }) {
     const handleCancel = () => {
         setIsOpen(false);
     };
+
+    const modalActionButtons = [
+        { label: 'Open card', icon: cardIcon, onClick: () => console.log('Add to X') },
+        { label: 'Edit labels', icon: labelIcon, onClick: () => console.log('Add to Y') },
+        { label: 'Change members', icon: userIcon, onClick: () => console.log('Add to Y') },
+        { label: 'Change cover', icon: coverIcon, onClick: () => console.log('Add to Y') },
+        { label: 'Edit date', icon: timeIcon, onClick: () => console.log('Add to Y') },
+        { label: 'More', icon: moreIcon, onClick: () => console.log('Add to Y') },
+        { label: 'Copy', icon: copyIcon, onClick: () => console.log('Add to Y') },
+        { label: 'Archive', icon: archiveIcon, onClick: () => console.log('Add to Y') },
+    ];
 
     return (
         <div>
@@ -68,6 +87,7 @@ export function TaskPreviewEditModal({ task, isHovered, editTask }) {
                         ))}
                     </article>
                     <TextArea
+                        className="task-name-input"
                         autoSize={{ minRows: 2, maxRows: 6 }}
                         value={taskName}
                         onChange={(e) => setTaskName(e.target.value)}
@@ -77,6 +97,20 @@ export function TaskPreviewEditModal({ task, isHovered, editTask }) {
                 <button className="floating-button save-button" onClick={handleOk}>
                     Save
                 </button>
+                <section className='floating-buttons'>
+                    {modalActionButtons.map((btn, index) => (
+                        <a key={index} className="floating-button" onClick={btn.onClick}>
+                            <ReactSVG
+                                src={btn.icon}
+                                beforeInjection={(svg) => {
+                                    svg.classList.add('task-action-icon');
+                                }}
+                                wrapper="span"
+                            />
+                            {btn.label}
+                        </a>
+                    ))}
+                </section>
             </Modal>
         </div>
     )
