b22eeaf74bd4ce563fd9b3660ca7b524b85662a6
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-06-30 12:35:20 +0300

    refactor task preview

--- src/cmps/Task/TaskPreview.jsx
+++ src/cmps/Task/TaskPreview.jsx
@@ -1,20 +1,14 @@
 import { Card } from "antd";
 import { utilService } from "../../services/util.service";
 import { TaskPreviewLabel } from "./TaskPreviewLabel";
-import { useSelector } from "react-redux";
-import { UserAvatar } from "../UserAvatar";
 import { TaskPreviewBadges } from "./TaskPreviewBadges";
 
 export function TaskPreview({ task }) {
-  const members = useSelector((state) => state.boardModule.members);
-
-  const taskMembers = members.filter((member) => task.idMembers.includes(member.id)) || [];
-
-  const taskMember = task.cover;
+  const taskCover = task.cover;
 
   return (
     <Card className={`group-task  custom-card `}>
-      {taskMember.color && (
+      {taskCover.color && (
         <div
           className="group-task-header"
           style={{
@@ -23,7 +17,7 @@ export function TaskPreview({ task }) {
             ),
           }}></div>
       )}
-      {taskMember.idUploadedBackground && (
+      {taskCover.idUploadedBackground && (
         <div
           className="group-task-header img-cover"
           style={{ backgroundImage: `url(${task.cover.scaled[2].url})` }}
@@ -36,18 +30,8 @@ export function TaskPreview({ task }) {
           ))}
         </article>
         <span className="group-task-content-title">{task.name}</span>
-        <div className="group-task-content-icons">
-          <aside className="aside-left">
-            <TaskPreviewBadges task={task} members={members} />
-          </aside>
-          <aside className="aside-right">
-            {taskMembers.map((member) => (
-              <UserAvatar key={member.id} member={member} />
-            ))}
-          </aside>
-        </div>
+        <TaskPreviewBadges task={task} />
       </section>
-
     </Card>
   );
 }
