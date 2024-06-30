86cf11cb0bb3597bddf6aefa6f9055633efcf4a4
votex001 <jeneea45@gmail.com>
2024-06-30 19:40:44 +0300

    chage name

--- src/cmps/BoardHeader-cmps/MemberSelect.jsx
+++ src/cmps/BoardHeader-cmps/MemberSelect.jsx
@@ -1,9 +1,13 @@
-import { DownOutlined } from "@ant-design/icons";
 import { Popover } from "antd";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
-export function CustomSelect() {
+//svg
+import checkedIcon from "/img/headerImgs/checkedIcon.svg";
+import { ReactSVG } from "react-svg";
+import { DownOutlined } from "@ant-design/icons";
+
+export function MemberSelect() {
   const [openListMenu, setOpenListMenu] = useState(false);
   const members = useSelector((state) => state.boardModule.members).filter(
     (member) => member.id !== "666fe4efda8643029b6710f3"
@@ -19,8 +23,9 @@ export function CustomSelect() {
       content={
         <>
           {members?.map((member) => (
-            <label htmlFor={member} key={member.id}>
+            <label htmlFor={member} key={member.id} className="custom-select">
               <input type="checkbox" id={member} />
+              <ReactSVG src={checkedIcon} className="checkbox" wrapper="span" />
               {member.fullName}
             </label>
           ))}
