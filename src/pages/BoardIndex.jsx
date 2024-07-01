ed23428d2e647343d85de95e6e053defa712ba54
votex001 <jeneea45@gmail.com>
2024-07-01 19:40:36 +0300

    wip

--- src/pages/BoardIndex.jsx
+++ src/pages/BoardIndex.jsx
@@ -93,7 +93,7 @@ export function BoardIndex() {
           <AddGroupBtn addGroup={onAddGroup} />
         </main>
       </div>
-      {clickedTaskId && <TaskDetailsModal taskId={clickedTaskId} />}
+      {/* {clickedTaskId && <TaskDetailsModal taskId={clickedTaskId} />} */}
     </section>
   ) : (
     <h1>Loading...</h1>
