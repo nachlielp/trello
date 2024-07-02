465177b7c8b5ef63b09576c0ab5446c49a9cf8d3
votex001 <jeneea45@gmail.com>
2024-07-02 17:26:07 +0300

    refactoring

--- src/cmps/CustomCpms/SvgButton.jsx
+++ src/cmps/CustomCpms/SvgButton.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { ReactSVG } from 'react-svg';
+import React from "react";
+import { ReactSVG } from "react-svg";
 
-export function SvgButton({ src, className, onClick, label }) {
-    return (
-        <button className={className} onClick={onClick} >
-            <ReactSVG
-                src={src}
-                wrapper="span"
-            />
-            {label && <span>{label}</span>}
-        </button>
-    );
+export function SvgButton({ src, label, ...other }) {
+  return (
+    <button {...other}>
+      <ReactSVG src={src} wrapper="span" />
+      {label}
+    </button>
+  );
 }
