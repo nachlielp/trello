d0939bf2ddacaccd5df6c7122ab695a5db3bd3fd
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-07-06 02:18:09 +0300

    Update CustomSelect.jsx

--- src/cmps/CustomCpms/CustomSelect.jsx
+++ src/cmps/CustomCpms/CustomSelect.jsx
@@ -3,85 +3,84 @@ import { Popover } from "antd";
 import { useState, useEffect, useRef } from "react";
 
 export function CustomSelect({ options = [], onSelect, currentSelect }) {
-  export function CustomSelect({ options = [], onSelect, currentSelect }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(options[0]);
-    const [searchValue, setSearchValue] = useState("");
-    const [filteredItems, setFilteredItems] = useState(options);
-    const divRef = useRef(null);
-    useEffect(() => {
-      onSelect(options.find((o) => o.id === currentSelect));
-    }, [currentSelect]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(options[0]);
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredItems, setFilteredItems] = useState(options);
+  const divRef = useRef(null);
+  useEffect(() => {
+    onSelect(options.find((o) => o.id === currentSelect));
+  }, [currentSelect]);
 
 
-    useEffect(() => {
-      onSelect(options.find(o => o.id === currentSelect))
-    }, [currentSelect])
+  useEffect(() => {
+    onSelect(options.find(o => o.id === currentSelect))
+  }, [currentSelect])
 
-    useEffect(() => {
-      setFilteredItems(options);
-      if (options.length > 0) {
-        setSelectedItem(options[0]);
-      }
-
-      if (currentSelect) {
-        setSelectedItem(options.find(o => o.id === currentSelect))
-      }
-    }, [options]);
-
-
-    function onInput(e) {
-      setSearchValue(e.target.value);
-
+  useEffect(() => {
+    setFilteredItems(options);
+    if (options.length > 0) {
+      setSelectedItem(options[0]);
     }
 
-    function onSelectOption(item) {
-
-      setSelectedItem(item);
-      if (onSelect) {
-        onSelect(item);
-      }
-      setIsOpen(false);
+    if (currentSelect) {
+      setSelectedItem(options.find(o => o.id === currentSelect))
     }
+  }, [options]);
 
-    return (
-      <Popover
-        trigger="click"
-        placement="bottomLeft"
-        open={isOpen}
-        onOpenChange={setIsOpen}
-        arrow={false}
-        content={
-          <div
-            className="options"
-            style={{ width: `${divRef.current?.clientWidth}px` }}
-          >
-            {filteredItems.map((item) => (
-              <button
-                key={item?.id}
-                onClick={() => onSelectOption(item)}
-                className={selectedItem?.name === item?.name ? "selected" : ""}
-              >
-                {item?.name}
-              </button>
-            ))}
-          </div>
-        }
-      >
-        <div
-          className="custom-select-item"
-          onClick={() => setIsOpen(!isOpen)}
-          ref={divRef}
-        >
-          {
-            <input
-              placeholder={selectedItem?.name || options[0]?.name}
-              value={searchValue}
-              onChange={onInput}
-            />
-          }
-          <DownOutlined className="arrow-down" />
-        </div>
-      </Popover>
-    );
+
+  function onInput(e) {
+    setSearchValue(e.target.value);
+
   }
+
+  function onSelectOption(item) {
+
+    setSelectedItem(item);
+    if (onSelect) {
+      onSelect(item);
+    }
+    setIsOpen(false);
+  }
+
+  return (
+    <Popover
+      trigger="click"
+      placement="bottomLeft"
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      arrow={false}
+      content={
+        <div
+          className="options"
+          style={{ width: `${divRef.current?.clientWidth}px` }}
+        >
+          {filteredItems.map((item) => (
+            <button
+              key={item?.id}
+              onClick={() => onSelectOption(item)}
+              className={selectedItem?.name === item?.name ? "selected" : ""}
+            >
+              {item?.name}
+            </button>
+          ))}
+        </div>
+      }
+    >
+      <div
+        className="custom-select-item"
+        onClick={() => setIsOpen(!isOpen)}
+        ref={divRef}
+      >
+        {
+          <input
+            placeholder={selectedItem?.name || options[0]?.name}
+            value={searchValue}
+            onChange={onInput}
+          />
+        }
+        <DownOutlined className="arrow-down" />
+      </div>
+    </Popover>
+  );
+}
