e492145b3446f96883f5c85d13d900b7d2dba1d0
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-07-07 11:58:20 +0300

    add label to svg button befor svg

--- src/cmps/CustomCpms/SvgButton.jsx
+++ src/cmps/CustomCpms/SvgButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ReactSVG } from "react-svg";
 
-export function SvgButton({ src, label, ...other }) {
+export function SvgButton({ src, label, preLabel, ...other }) {
   return (
     <button {...other}>
+      <span className="pre-label">{preLabel}</span>
       <ReactSVG src={src} wrapper="span" />
-      {label}
+      <span className="label">{label}</span>
     </button>
   );
 }
