e0770e3fbd6416631c9753f25e659b92befce157
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-07-09 21:10:00 +0300

    add formating button to markdown component

--- src/cmps/Task/TaskDetailsModal/TaskDetailsMarkdown.jsx
+++ src/cmps/Task/TaskDetailsModal/TaskDetailsMarkdown.jsx
@@ -68,10 +68,16 @@ export function TaskDetailsMarkdown({ editTask, task }) {
                     ]}
                 />
                 }
-                {isEditing && <footer className='markdown-footer'>
-                    <button className='btn btn-action' onClick={onSave}>Save</button>
-                    <button className='btn btn-secondary' onClick={onCancel}>Cancel</button>
-                </footer>}
+                {isEditing &&
+                    <footer className='markdown-footer'>
+                        <article className='markdown-footer-buttons'>
+                            <button className='btn btn-action' onClick={onSave}>Save</button>
+                            <button className='btn btn-secondary' onClick={onCancel}>Cancel</button>
+                        </article>
+                        <article className='markdown-footer-links'>
+                            <a className='btn btn-primary' href='https://support.atlassian.com/trello/docs/how-to-format-your-text-in-trello/' target='_blank'>Formatting help</a>
+                        </article>
+                    </footer>}
             </main>
         </section>
     );
