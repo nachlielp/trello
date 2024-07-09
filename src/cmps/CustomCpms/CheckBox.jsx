7dd3a332349227e7304b474f7fc526108facb206
votex001 <jeneea45@gmail.com>
2024-07-09 21:43:23 +0300

    renaming

--- src/cmps/CustomCpms/CheckBox.jsx
+++ src/cmps/CustomCpms/CheckBox.jsx
@@ -4,12 +4,12 @@ import checkedIcon from "/img/board-index/headerImgs/checkedIcon.svg";
 
 export function CheckBox({ className, onChange, ...other }) {
   return (
-    <label className={className}>
-      <input type="checkBox" onChange={onChange} {...other} />
+    <label className={`custom-checkbox ${className?className:""}`}>
+      <input className="input-checkbox" type="checkBox" onChange={onChange} {...other} />
       <ReactSVG
         src={checkedIcon}
         wrapper="span"
-        className={`checkbox`}
+        className='checkbox'
       />
     </label>
   );
