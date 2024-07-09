7aaadac58dc8a0b8718bd7ae8e030f4a24e2a0fc
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-07-09 23:36:15 +0300

    Update TaskDetailsLabels.jsx

--- src/cmps/Task/TaskDetailsModal/TaskDetailsLabels.jsx
+++ src/cmps/Task/TaskDetailsModal/TaskDetailsLabels.jsx
@@ -1,17 +1,20 @@
 import { SvgButton } from "../../CustomCpms/SvgButton";
 import { ManageLabelsPopover } from "../ManageTaskPopovers/ManageLabelsPopover";
-
+import { useSelector } from "react-redux";
+import { utilService } from "../../../services/util.service";
 export function TaskDetailsLabels({ task, editTask, editLabel }) {
+    const boardLabels = useSelector((state) => state.boardModule.board.labelNames);
     return (
         <section className="task-details-labels">
             <p className="sub-title">Labels</p>
             <article className="label-list">
-                {task.labels.map((label) => (
-
-                    <div className="task-details-label" key={label.color} style={{ backgroundColor: label.color }}>
-                        {label.label}
-                    </div>
-                ))}
+                {task.labels.map((tLabel) => {
+                    const labelInfo = boardLabels?.find((bLabel) => bLabel.color === tLabel.color)
+                    return (
+                        <div className="task-details-label" key={labelInfo.color} style={{ backgroundColor: utilService.getColorHashByName(labelInfo.color).bgColor }}>
+                            {labelInfo.label}
+                        </div>)
+                })}
                 <ManageLabelsPopover editTask={editTask} editLabel={editLabel} task={task} anchorEl={popoverAncher} />
             </article>
         </section>
