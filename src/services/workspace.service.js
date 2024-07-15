6f0ddec055fc3d1746498d441c4e548cec6136fe
votex001 <jeneea45@gmail.com>
2024-07-15 23:37:47 +0300

    Update workspace.service.js

--- src/services/workspace.service.js
+++ src/services/workspace.service.js
@@ -2,12 +2,11 @@ import { store } from "../store/store";
 import { storageService } from "./async-storage.service";
 import { BOARDS_KEY } from "./util.service";
 
-export const workspaceService ={
-    getAllBoards,
+export const workspaceService = {
+  getAllBoards,
+};
+
+function getAllBoards() {
+  return storageService.query(BOARDS_KEY);
 }
 
-
-function getAllBoards(){
-    return storageService.query(BOARDS_KEY)
-    store.dispat
-}
