278b9005771baa3ada3d923dd058551280c059f3
votex001 <jeneea45@gmail.com>
2024-07-26 00:40:44 +0300

    checking darkmode

--- src/pages/WorkspaceIndex.jsx
+++ src/pages/WorkspaceIndex.jsx
@@ -3,115 +3,180 @@ import { WorkspaceHeader } from "../cmps/Workspace/WorkspaceHeader";
 import { WorkspaceMenu } from "../cmps/Workspace/WorkspaceMenu";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { setBoard, loadBoard, loadBoardByTaskId, viewBoard } from "../store/board.actions";
-import { login, editUser, addBoardToUser, loadUsers } from "../store/user.actions";
+import {
+  setBoard,
+  loadBoard,
+  loadBoardByTaskId,
+  viewBoard,
+} from "../store/board.actions";
+import {
+  login,
+  editUser,
+  addBoardToUser,
+  loadUsers,
+} from "../store/user.actions";
 import { createBoard } from "../store/workspace.actions";
 import { useSelector } from "react-redux";
-import { setBoards, editWorkspaceBoardState } from "../store/workspace.actions"
+import { setBoards, editWorkspaceBoardState } from "../store/workspace.actions";
 import { updateBoard } from "../store/board.actions";
 import { UserBoards } from "./UserBoards";
 
 export function WorkspaceIndex() {
-    const user = useSelector((state) => state.userModule.user);
-    const boardsInfo = useSelector((state) => state.workspaceModule.boards)
-        ?.filter(b => user && b?.members?.some(m => m.id === user.id)).filter((b) => !b.closed)
-        .map((b) => ({ id: b.id, name: b.name, closed: b.closed, coverImg: b.prefs.backgroundImage }));
-    const boards = useSelector((state) => state.workspaceModule.boards);
-    const boardBgPrefs = useSelector((state) => state.boardModule.board)?.prefs;
+  const user = useSelector((state) => state.userModule.user);
+  const boardsInfo = useSelector((state) => state.workspaceModule.boards)
+    ?.filter((b) => user && b?.members?.some((m) => m.id === user.id))
+    .filter((b) => !b.closed)
+    .map((b) => ({
+      id: b.id,
+      name: b.name,
+      closed: b.closed,
+      coverImg: b.prefs.backgroundImage,
+    }));
+  const boards = useSelector((state) => state.workspaceModule.boards);
+  const boardBgPrefs = useSelector((state) => state.boardModule.board)?.prefs;
 
-    const [selectedBoardId, setSelectedBoardId] = useState(null);
-    const [starredBoardIds, setStarredBoardIds] = useState([]);
-    const [isUserBoards, setIsUserBoards] = useState(false);
-
-    const navigate = useNavigate();
-    const params = useParams();
-
-    useEffect(() => {
-        setBoards();
-        login();
-        loadUsers()
-    }, []);
-
-
-
-
-    useEffect(() => {
-        if (params.boardId) {
-            loadBoard(params.boardId);
-            setSelectedBoardId(params.boardId);
-            setIsUserBoards(false);
-            viewBoard(params.boardId);
-        }
-        if (params.cardId) {
-            loadBoardByTaskId(params.cardId).then((boardId) => {
-                setSelectedBoardId(boardId);
-                setIsUserBoards(false);
-                viewBoard(boardId);
-            });
-        }
-    }, [params]);
-
-    //Notice any change in user page is through this 
-    //Make sure that changes dont break navigation
-    useEffect(() => {
-        if (!params.boardId && !params.cardId && user && !isUserBoards) {
-         
-            setSelectedBoardId(null);
-       
-        }
-    }, [params, user, isUserBoards]);
-
-    useEffect(() => {
-        setStarredBoardIds(user?.starredBoardIds);
-    }, [user]);
-
-
-    function onStarClick(boardId) {
-        const isStarred = user.starredBoardIds.includes(boardId);
-        const starredBoardIds = isStarred ? user.starredBoardIds.filter((id) => id !== boardId) : [...user.starredBoardIds, boardId];
-        editUser({ ...user, starredBoardIds });
+  const [selectedBoardId, setSelectedBoardId] = useState(null);
+  const [starredBoardIds, setStarredBoardIds] = useState([]);
+  const [isUserBoards, setIsUserBoards] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    // Initialize state based on localStorage or system preference
+    const savedMode = localStorage.getItem("dark");
+    if (savedMode !== null) {
+      return savedMode === "true";
     }
+    // Default to system preference if no saved mode
+    return null;
+  });
 
-    async function onAddBoard(board) {
-        const boardId = await createBoard(board);
-        await addBoardToUser(boardId);
-        navigate(`/b/${boardId}`);
-    }
+  const navigate = useNavigate();
+  const params = useParams();
 
-    function onCloseBoard(boardId) {
-        console.log("onCloseBoard", boardId);
-        const board = boards.find((b) => b.id === boardId);
-        if (board) {
-            updateBoard({ ...board, closed: true });
-        }
-    }
+  useEffect(() => {
+    setBoards();
+    login();
+    loadUsers();
+  }, []);
 
-    function onLeaveBoard(boardId) {
-        const board = boards.find((b) => b.id === boardId);
-        if (board) {
-            updateBoard({ ...board, members: board.members.filter((m) => m.id !== user.id) });
-            editUser({ ...user, idBoards: user.idBoards.filter((id) => id !== boardId) });
-        }
+  useEffect(() => {
+    if (params.boardId) {
+      loadBoard(params.boardId);
+      setSelectedBoardId(params.boardId);
+      setIsUserBoards(false);
+      viewBoard(params.boardId);
     }
-    return (
-        <section className={`workspace ${isUserBoards ? "user-boards-bg" : ""}`} style={{
-            backgroundImage: selectedBoardId && boardBgPrefs?.backgroundImage ? `url(${boardBgPrefs.backgroundImage})` : 'none',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-        }}>
-            <WorkspaceHeader bgColor={selectedBoardId && boardBgPrefs?.backgroundColor || ""} userName={user?.username} />
-            {(user && starredBoardIds && selectedBoardId) ?(
-                <section className="workspace-content">
-                    <WorkspaceMenu colorTheme={boardBgPrefs?.backgroundBrightness} boardsInfo={boardsInfo} selectedBoardId={selectedBoardId} starredBoardIds={starredBoardIds} onStarClick={onStarClick} onAddBoard={onAddBoard} closeBoard={onCloseBoard} leaveBoard={onLeaveBoard} />
-                    <Outlet />
-                </section>
-            ):<Outlet />}
-           
-            {!user && (
-                <div>Loading...</div>
-            )}
+    if (params.cardId) {
+      loadBoardByTaskId(params.cardId).then((boardId) => {
+        setSelectedBoardId(boardId);
+        setIsUserBoards(false);
+        viewBoard(boardId);
+      });
+    }
+  }, [params]);
+
+  //Notice any change in user page is through this
+  //Make sure that changes dont break navigation
+  useEffect(() => {
+    if (!params.boardId && !params.cardId && user && !isUserBoards) {
+      setSelectedBoardId(null);
+    }
+  }, [params, user, isUserBoards]);
+
+  useEffect(() => {
+    setStarredBoardIds(user?.starredBoardIds);
+  }, [user]);
+
+  useEffect(() => {
+    // Update localStorage and <html> class based on darkMode state
+    if (darkMode === true) {
+      localStorage.setItem("dark", "true");
+      document.querySelector("html").classList.add("dark");
+    } else if (darkMode === false) {
+      localStorage.setItem("dark", "false");
+      document.querySelector("html").classList.remove("dark");
+    }
+    if (darkMode === null) {
+      localStorage.removeItem("dark");
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.querySelector("html").classList.add("dark");
+      } else if (!window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.querySelector("html").classList.remove("dark");
+      }
+    }
+  }, [darkMode]);
+
+  function onStarClick(boardId) {
+    const isStarred = user.starredBoardIds.includes(boardId);
+    const starredBoardIds = isStarred
+      ? user.starredBoardIds.filter((id) => id !== boardId)
+      : [...user.starredBoardIds, boardId];
+    editUser({ ...user, starredBoardIds });
+  }
+
+  async function onAddBoard(board) {
+    const boardId = await createBoard(board);
+    await addBoardToUser(boardId);
+    navigate(`/b/${boardId}`);
+  }
+
+  function onCloseBoard(boardId) {
+    console.log("onCloseBoard", boardId);
+    const board = boards.find((b) => b.id === boardId);
+    if (board) {
+      updateBoard({ ...board, closed: true });
+    }
+  }
+
+  function onLeaveBoard(boardId) {
+    const board = boards.find((b) => b.id === boardId);
+    if (board) {
+      updateBoard({
+        ...board,
+        members: board.members.filter((m) => m.id !== user.id),
+      });
+      editUser({
+        ...user,
+        idBoards: user.idBoards.filter((id) => id !== boardId),
+      });
+    }
+  }
+  return (
+    <section
+      className={`workspace ${isUserBoards ? "user-boards-bg" : ""}`}
+      style={{
+        backgroundImage:
+          selectedBoardId && boardBgPrefs?.backgroundImage
+            ? `url(${boardBgPrefs.backgroundImage})`
+            : "none",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <WorkspaceHeader
+        bgColor={(selectedBoardId && boardBgPrefs?.backgroundColor) || ""}
+        userName={user?.username}
+        setDarkMode={setDarkMode}
+        darkMode={darkMode}
+      />
+      {user && starredBoardIds && selectedBoardId ? (
+        <section className="workspace-content">
+          <WorkspaceMenu
+            colorTheme={boardBgPrefs?.backgroundBrightness}
+            boardsInfo={boardsInfo}
+            selectedBoardId={selectedBoardId}
+            starredBoardIds={starredBoardIds}
+            onStarClick={onStarClick}
+            onAddBoard={onAddBoard}
+            closeBoard={onCloseBoard}
+            leaveBoard={onLeaveBoard}
+          />
+          <Outlet />
         </section>
-    
-    );
+      ) : (
+        <Outlet />
+      )}
+
+      {!user && <div>Loading...</div>}
+    </section>
+  );
 }
