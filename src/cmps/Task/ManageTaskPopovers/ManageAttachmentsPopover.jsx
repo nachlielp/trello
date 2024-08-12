fa48c8aad1ee238b24a06186e1a71f642158a7e8
Nachliel <nachpok@gmail.com>
2024-08-12 23:35:30 +0300

    add attachments popover UI

--- src/cmps/Task/ManageTaskPopovers/ManageAttachmentsPopover.jsx
+++ src/cmps/Task/ManageTaskPopovers/ManageAttachmentsPopover.jsx
@@ -1,12 +1,9 @@
-import { Popover, Calendar, Card, Input, Checkbox } from "antd";
+import { Popover, Input } from "antd";
 import { ManageTaskPopoverHeader } from "./ManageTaskPopoverHeader";
-import dayjs from "dayjs";
 import { useState, useRef, useEffect } from "react";
-import { SvgButton } from "../../CustomCpms/SvgButton";
-import { CustomSelect } from "../../CustomCpms/CustomSelect";
+import { Tooltip } from "antd";
+import { CloseCircleOutlined } from "@ant-design/icons";
 import { utilService } from "../../../services/util.service";
-import { useSelector } from "react-redux";
-
 export function ManageAttachmentsPopover({
   anchorEl,
   task,
@@ -22,18 +19,13 @@ export function ManageAttachmentsPopover({
   return (
     <Popover
       open={isOpen}
+      onClose={onClose}
       onOpenChange={setIsOpen}
       trigger="click"
       anchorEl={anchorEl}
       anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
       transformOrigin={{ vertical: "top", horizontal: "left" }}
       content={
-        // <ManageDatesPopoverContent
-        //   editBoard={editBoard}
-        //   task={task}
-        //   editTask={editTask}
-        //   onClose={onClose}
-        // />
         <ManageAttachmentsPopoverContent
           task={task}
           editTask={editTask}
@@ -48,5 +40,133 @@ export function ManageAttachmentsPopover({
 }
 
 function ManageAttachmentsPopoverContent({ task, editTask, onClose }) {
-  return <div>ManageAttachmentsPopoverContent</div>;
+  const [link, setLink] = useState("");
+  const [text, setText] = useState("");
+  const [invalidLink, setInvalidLink] = useState(false);
+  const [focusedLink, setFocusedLink] = useState(false);
+  const [focusedText, setFocusedText] = useState(false);
+
+  const linkRef = useRef(null);
+  const textRef = useRef(null);
+
+  function onAddLink() {
+    if (link === "" || !utilService.isValidUrl(link)) {
+      setInvalidLink(true);
+      return;
+    }
+    editTask({
+      ...task,
+      attachments: [...task.attachments, { link, text }],
+    });
+    onClose();
+  }
+  function onClearLink() {
+    setLink("");
+    linkRef.current.focus();
+  }
+
+  function onClearText() {
+    setText("");
+    textRef.current.focus();
+  }
+
+  return (
+    <section className="manage-attachments-popover-content">
+      <ManageTaskPopoverHeader
+        title="Attachments"
+        close={onClose}
+        // onSave={onSave}
+      />
+      <main className="main">
+        <label className="section-title">
+          Attach a file from your computer
+        </label>
+        <label className="section-subtitle">
+          You can also drag and drop files to upload them.
+        </label>
+        <button className="btn upload-button">Upload</button>
+        <hr className="divider" />
+        <label className="section-title">Search or paste a link</label>
+        <div className="input-wrapper">
+          <Input
+            type="text"
+            className={`input ${invalidLink && "invalid"} ${
+              focusedLink && "focused"
+            }`}
+            placeholder="Find recent links or past a new link"
+            suffix={
+              <ClearLinkIcon onClick={onClearLink} display={link !== ""} />
+            }
+            value={link}
+            onChange={(e) => {
+              setLink(e.target.value);
+              setInvalidLink(false);
+            }}
+            onFocus={() => setFocusedLink(true)}
+            onBlur={() => setFocusedLink(false)}
+            ref={linkRef}
+          />
+          {invalidLink && (
+            <label className="invalid-link-message">
+              <ErrorIcon />
+              Enter a valid URL.
+            </label>
+          )}
+        </div>
+        <label className="section-title">Display text (optional)</label>
+        <div className="input-wrapper">
+          <Input
+            type="text"
+            className={`input ${focusedText && "focused"}`}
+            placeholder="Text to display"
+            suffix={
+              <ClearLinkIcon onClick={onClearText} display={text !== ""} />
+            }
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onFocus={() => setFocusedText(true)}
+            onBlur={() => setFocusedText(false)}
+            ref={textRef}
+          />
+        </div>
+        {/* <label className="section-title">Recently viewed</label> */}
+        {/* TODO: Add recently viewed */}
+        {/* <article className="recently-viewed"></article> */}
+        <footer className="footer">
+          <button className="btn cancel">Cancel</button>
+          <button className="btn insert" onClick={onAddLink}>
+            Insert
+          </button>
+        </footer>
+      </main>
+    </section>
+  );
 }
+
+const ClearLinkIcon = ({ display, onClick }) => {
+  return (
+    <Tooltip title="Clear link" arrow={false} placement="bottom">
+      <div
+        className="close-icon-wrapper"
+        style={{
+          background: "44546f",
+          padding: "4px",
+          display: display ? "flex" : "none",
+        }}
+        onClick={onClick}
+      >
+        <label className="trello-icon icon-close"></label>
+      </div>
+    </Tooltip>
+  );
+};
+
+const ErrorIcon = () => {
+  return (
+    <label className="error-icon-wrapper">
+      <label className="attachment-error-icon">
+        <label className="attachment-error-icon-text">!</label>
+      </label>
+    </label>
+  );
+};
