68fe56b5f28a6bfc2cd625f072d5651c5519e736
votex001 <jeneea45@gmail.com>
2024-08-25 21:18:20 +0300

    Update BoardActivity.jsx

--- src/cmps/BoardHeader/BoardMenu/BoardActivity.jsx
+++ src/cmps/BoardHeader/BoardMenu/BoardActivity.jsx
@@ -4,7 +4,9 @@ import { ActivityMsg } from "../../ActivityMsg";
 export function BoardActivity() {
   const board = useSelector((state) => state.boardModule.board);
   if (!board.activities.length) return;
-  return board.activities.sort((a,b)=>{return b.timeStamp - a.timeStamp}).map((activity) => (
-    <ActivityMsg activity={activity} key={activity.id} />
-  ));
+  return board.activities
+    .sort((a, b) => {
+      return b.timeStamp - a.timeStamp;
+    })
+    .map((activity) => <ActivityMsg activity={activity} key={activity.id} />);
 }
