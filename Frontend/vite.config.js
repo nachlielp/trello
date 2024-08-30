292b35f1d7cea9a00a92297b6078a9e8ac3a8fc5
Nachliel <nachpok@gmail.com>
2024-08-30 20:57:00 +0300

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "../Backend/public",
     assetsDir: "assets",
   },
   server: {
