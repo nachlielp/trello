71e1f12ddc96a2f5b7972975164d88e2e3045322
votex001 <jeneea45@gmail.com>
2024-09-02 13:35:27 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,15 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { authRoutes } from "./api/auth/auth.routes.js";
 import { userRouter } from "./api/user/user.routes.js";
 import { boardRouter } from "./api/board/board.routes.js";
+
+// Get the current directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
@@ -19,22 +25,26 @@ const corsOptions = {
   ],
   credentials: true,
 };
-//config
-app.use(express.static("public"));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
-//Routes
+// API Routes
 app.use("/api/boards", boardRouter);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRouter);
 
-app.get("/**", (req, res) => {
-  res.sendFile(path.resolve("public/index.html"));
+// Handle all other routes by sending 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 const port = 3030;
 server.listen(port, () => {
-  console.log(`Server listening on port http://127.0.0.1:${port}`);
+  console.log(`Server listening on http://127.0.0.1:${port}`);
 });
