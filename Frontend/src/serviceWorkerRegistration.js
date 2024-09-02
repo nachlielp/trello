38169f2feec4f7a68fa67d6e668432ea5d242478
votex001 <jeneea45@gmail.com>
2024-09-02 12:49:33 +0300

    debug

--- Frontend/src/serviceWorkerRegistration.js
+++ Frontend/src/serviceWorkerRegistration.js
@@ -30,7 +30,8 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const baseUrl = process.env.PUBLIC_URL || '';
+        const swUrl = `${baseUrl}/service-worker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
